51914aa17368bc1b3c10dfe0a5cd3c2f379c273a
Lauther Valladares <lautherharold27@gmail.com>
2022-11-12 16:01:05 -0500

    error advise

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,6 +8,7 @@ import useModal from '../hooks/useModal';
 const Form = ({ getData, selectedUser, unSelectUser }) => {
     const { handleSubmit, register, reset } = useForm()
     const [isOpen, openModal, closeModal] = useModal()
+    const [isError, setIsError] = useState(null)
 
     const initialValues = {
         email: '',
@@ -28,27 +29,48 @@ const Form = ({ getData, selectedUser, unSelectUser }) => {
     }, [selectedUser])
 
     const closeButtonFunction = () => {
-        reset(initialValues)
-        closeModal()
-        unSelectUser()
+        reset(initialValues);
+        closeModal();
+        unSelectUser();
+        setIsError(null);
     }
 
     const submit = (data) => {
         if (selectedUser) {
             axios.put(`https://users-crud1.herokuapp.com/users/${selectedUser.id}/`, data)
                 .then(() => {
-                    getData(),
-                        unSelectUser(),
-                        closeModal()
+                    getData();
+                    closeModal();
+                    unSelectUser();
+                    setIsError(null);
+                })
+                .catch(error => {
+                    setIsError(error);
                 })
         } else {
             axios.post('https://users-crud1.herokuapp.com/users/', data)
-                .then(() =>
+                .then(() => {
                     getData(),
                     reset(initialValues),
-                    closeModal())
-                .catch(error => console.log(error.response))
+                    closeModal();
+                    unSelectUser();
+                    setIsError(null);
+                }
+                )
+                .catch(error => {
+                    setIsError(error)
+                })
         }
+    }
+
+    function setModalState() {
+        let state = ''
+        if (isError) {
+            state = 'modalOn'
+        } else {
+            isOpen ? state = 'modalOn' : null
+        }
+        return state
     }
 
     return (
@@ -58,33 +80,38 @@ const Form = ({ getData, selectedUser, unSelectUser }) => {
                 Create new user
             </button>
             <form className='users-form' onSubmit={handleSubmit(submit)}>
-                <div className={`modal-form ${isOpen ? 'modalOn' : null}`} >
+                <div className={`modal-form ${setModalState()}`} >
                     <div className='create-container'>
                         <h1>{selectedUser ? 'Edit user' : 'New user'}</h1>
                         <button type='button' className='btn close' onClick={closeButtonFunction}>
                             <i className="fa-solid fa-x fa-xs"></i>
                         </button>
                         <div className='feature'>
-                            <label htmlFor="name">Name</label>
+                            <label htmlFor="name">Name*</label>
                             <input {...register('first_name')} type="text" id="name" />
                         </div>
                         <div className='feature'>
-                            <label htmlFor="lastName">Last Name</label>
+                            <label htmlFor="lastName">Last Name*</label>
                             <input {...register('last_name')} type="text" id="lastName" />
                         </div>
                         <div className='feature'>
-                            <label htmlFor="email">e-mail</label>
+                            <label htmlFor="email">e-mail*</label>
                             <input {...register('email')} type="email" id="email" />
                         </div>
                         <div className='feature'>
-                            <label htmlFor="password">Password</label>
+                            <label htmlFor="password">Password*</label>
                             <input {...register('password')} type="password" id="password" />
                         </div>
                         <div className='feature'>
-                            <label htmlFor="birthday">Birthday</label>
+                            <label htmlFor="birthday" >Birthday*</label>
                             <input {...register('birthday')} type="date" id="birthday" />
                         </div>
-                        <button className='btn add'>{selectedUser ? 'Save changes' : 'Create a user'}</button>
+                        <div className='error-alert' style={isError? {display: 'flex'} : null}>
+                            <p>Debe llenar los campos con *</p>
+                        </div>
+                        <button className='btn add'>
+                            {selectedUser ? 'Save changes' : 'Create a user'}
+                        </button>
                     </div>
                 </div>
             </form>
